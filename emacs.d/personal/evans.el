f019b81a41996a9374c98f52bb24cc6308311dcc
Paul Evans <evansp2@gmail.com>
2022-06-05 23:11:10 -0400

    updates

--- emacs.d/personal/evans.el
+++ emacs.d/personal/evans.el
@@ -1,11 +1,14 @@
-;;(require 'package)
-;;(add-to-list 'package-archives
-             ;;'("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;;(package-initialize)
+(add-hook 'prelude-prog-mode-hook (lambda () (smartparens-mode -1)) t)
 
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'clojure-mode-hook #'paredit-mode)
-(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'clojure-mode-hook          #'enable-paredit-mode)
+(add-hook 'nrepl-connected-hook       #'enable-paredit-mode)
 
 ; https://github.com/purcell/exec-path-from-shell
 (when (memq window-system '(mac ns x))
